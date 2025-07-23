9973d594bb1e43903a0430d99a8d289708bd115f
SleuthBear <76827575+SleuthBear@users.noreply.github.com>
2025-07-23 19:44:40 +1000

    Added hotkeys for opening apps

--- hotkeys.c
+++ hotkeys.c
@@ -2,21 +2,23 @@
 #include "constants.h"
 
 void hotkey1() {
-    printf("Hotkey 1 triggered\n");
+    printf("Redirecting to CLion\n");
+    system("open /Applications/CLion.app");
 }
 void hotkey2() {
-    printf("Hotkey 2 triggered\n");
+    printf("Redirecting to LibreWolf\n");
+    system("open /Application/LibreWolf.app");
 }
 
 KeyCodes keyCodes[2] = {
     {
         hotkey1,
-        {(1 << KEY_A) | (1 << KEY_S), 0},
+        {(1 << KEY_C) | (1 << KEY_L), 0},
    (kCGEventFlagMaskShift)
     },
     {
         hotkey2,
-        {(1 << KEY_A) | (1 << KEY_D), 0},
+        {(1 << KEY_B), 0},
    (kCGEventFlagMaskShift)
     }
 };
